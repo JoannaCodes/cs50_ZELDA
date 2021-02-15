076268f95184c63ea6da674100b6c16b1e8b7909
JoannaCodes <joannalara21@gmail.com>
2021-02-15 15:39:23 +0800

    Update PlayState.lua
    Changed names for Pot States

--- zelda/zelda/src/states/game/PlayState.lua
+++ zelda/zelda/src/states/game/PlayState.lua
@@ -34,7 +34,8 @@ function PlayState:init()
         ['idle'] = function() return PlayerIdleState(self.player) end,
         ['swing-sword'] = function() return PlayerSwingSwordState(self.player, self.dungeon) end,
         ['pot-lift'] = function () return PlayerPotLiftState(self.player, self.dungeon) end,
-        ['walk-pot-lift'] = function () return PlayerWalkPotLiftState(self.player, self.dungeon) end
+        ['pot-idle'] = function () return PlayerPotIdleState(self.player, self.dungeon) end,
+        ['pot-walk'] = function () return PlayerPotWalkState(self.player, self.dungeon) end
     }
     self.player:changeState('idle')
 end

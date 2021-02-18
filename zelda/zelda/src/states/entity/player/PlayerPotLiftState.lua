b6d049c34aecf2ac550153b2ba878870e86a187c
JoannaCodes <joannalara21@gmail.com>
2021-02-18 11:20:56 +0800

    Update PlayerPotLiftState.lua
    Fixed lift animation bug

--- zelda/zelda/src/states/entity/player/PlayerPotLiftState.lua
+++ zelda/zelda/src/states/entity/player/PlayerPotLiftState.lua
@@ -4,32 +4,24 @@ function PlayerPotLiftState:init(player, dungeon)
     self.player = player
     self.dungeon = dungeon
 
-    self.player.offsetY = 5
-    self.player.offsetX = 7
-
     self.player:changeAnimation('lift-pot-' .. self.player.direction)
 end
 
 
 function PlayerPotLiftState:update(dt)
+    -- pot lift
     for k, object in pairs(self.dungeon.currentRoom.objects) do
-        if object.type == 'pot' and self.player:collides(object) then
-            object.x = self.player.x - (self.player.width / 2)
-            object.y = self.player.y - (self.player.height / 2)
-        else
-            self.player:changeState('idle')
+        if object.type == 'pot' and object:collides(self.player) then
+            if self.player.currentAnimation.timesPlayed > 0 then
+                self.player.currentAnimation.timesPlayed = 0
+                self.player:changeState('pot-idle')
+            end
         end
     end
 
-
-    if self.player.currentAnimation.timesPlayed > 0 then
-        self.player.currentAnimation.timesPlayed = 0
-        self.player:changeState('pot-idle')
-    end
-
     if love.keyboard.isDown('left') or love.keyboard.isDown('right') or
-       love.keyboard.isDown('up') or love.keyboard.isDown('down') then
-        self.entity:changeState('pot-walk')
+        love.keyboard.isDown('up') or love.keyboard.isDown('down') then
+        self.player:changeState('pot-walk')
     end
 end
 
